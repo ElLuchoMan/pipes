4575dc9bd8f6c17e9f9bd12ba81bbc8d5b56c776
ElLuchoMan <luchoman96@gmail.com>
2021-02-03 20:39:15 -0500

    Fin del uso de pipes propios de angular

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+export class NoComunesComponent {
+  //i18nSelect
+  nombre: string = "Susana";
+  genero: string = "femenino";
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
   }
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Armando', 'Edwin', 'Bryan']
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+  cambiarCliente() {
+    this.nombre = 'Bryan',
+      this.genero = 'masculino'
+  }
+  borrarCliente() {
+    this.clientes.shift();
+  }
+  //KeyValue Pipe
+  persona={
+    nombre:'Bryan',
+    edad: 24,
+    direccion: 'Bogotá'
+  }
+  //JSon Pipe
+  heroes=[
+    {nombre: 'Capitan América', equipo: 'Los vengadores'},
+    {nombre: 'Iron Man', equipo: 'Los vengadores'},
+    {nombre: 'Batman', equipo: 'La liga de la Justicia'},
+    {nombre: 'Super Man', equipo: 'La liga de la Justicia'},
+  ]
+  //Async pipe
+myObservable = interval(1500); 
+valorPromise = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    resolve('Información Cargada');
+  },3500);
+}) 
 
 }
+
