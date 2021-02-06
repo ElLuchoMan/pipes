3168b6286de8a433398013e3e2dfa5c8f056fcee
ElLuchoMan <luchoman96@gmail.com>
2021-02-05 20:58:04 -0500

    Agregada tabla de heroes

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   ]
 })
 export class OrdenarComponent {
+  heroes: Heroe[] = [
+    { nombre: 'Superman', vuela: true, color: Color.azul },
+    { nombre: 'Batman', vuela: false, color: Color.negro },
+    { nombre: 'Robin', vuela: false, color: Color.verde },
+    { nombre: 'Daredevil', vuela: false, color: Color.rojo },
+    { nombre: 'Ironman', vuela: true, color: Color.rojo },
+    { nombre: 'Capitán América', vuela: false, color: Color.azul },
+    { nombre: 'Spiderman', vuela: false, color: Color.rojo },
+    { nombre: 'Scarlet Witch', vuela: true, color: Color.rojo }];
   enMayusculas: boolean = true;
   cambiar() {
     this.enMayusculas = !this.enMayusculas;
---
+++ src/app/ventas/pipes/vuela.pipe.ts
@@ -0,0 +1,9 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name:'vuela'
+})
+
+export class VuelaPipe implements PipeTransform{
+
+}
